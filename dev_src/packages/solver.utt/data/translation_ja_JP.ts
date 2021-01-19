4fa32d47f32708ac045d3eec8afb1bc7b354f231
Keisuke Inoue <kskinoue@gmail.com>
2021-01-19 09:58:40 +0900

    Solvers updated.

---
+++ dev_src/packages/solver.utt/data/translation_ja_JP.ts
@@ -0,0 +1,412 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TS version="2.0" language="ja_JP">
+    <!--これは iRIC で使用する辞書ファイルです。
+<translation> と </translation> の間に、翻訳後の文字列を追記してください。
+-->
+    <context>
+        <message>
+            <source>UTT</source>
+            <translation>UTT とても便利な多機能トレーサー追跡ツール・魚も追跡できます</translation>
+        </message>
+        <message>
+            <source>Basic Setting</source>
+            <translation>基本設定</translation>
+        </message>
+        <message>
+            <source>Flow information input file name</source>
+            <translation>流れの計算結果を読み込むCGNSファイル</translation>
+        </message>
+        <message>
+            <source>Normal Tracers</source>
+            <translation>通常のトレーサー</translation>
+        </message>
+        <message>
+            <source>Release Tracers</source>
+            <translation>投入</translation>
+        </message>
+        <message>
+            <source>(0) No</source>
+            <translation>しない</translation>
+        </message>
+        <message>
+            <source>(1) Yes</source>
+            <translation>する</translation>
+        </message>
+        <message>
+            <source>Maximum number of total tracer particles</source>
+            <translation>最大個数</translation>
+        </message>
+        <message>
+            <source>Maximum number of tracer particles in one cell</source>
+            <translation>１セルあたりの最大個数</translation>
+        </message>
+        <message>
+            <source>Special Tracers for Trajectory Tracking</source>
+            <translation>軌跡表示をする特別トレーサー</translation>
+        </message>
+        <message>
+            <source>Release Tracers for Trajectory</source>
+            <translation>投入</translation>
+        </message>
+        <message>
+            <source>Maximum number of total tracer particles for Trajectory</source>
+            <translation>特別トレーサーの最大個数</translation>
+        </message>
+        <message>
+            <source>Maximum Times of Tracer Trajectory Saving</source>
+            <translation>軌跡を保存する際の最大の時間ステップ数</translation>
+        </message>
+        <message>
+            <source>Interval of Tracer Trajectory Saving</source>
+            <translation>軌跡を保存する際の間引き間隔</translation>
+        </message>
+        <message>
+            <source>Velocity used for tracer tracking</source>
+            <translation>トレーサーの軌跡追跡に使用される流速</translation>
+        </message>
+        <message>
+            <source>(0) depth averaged velocity</source>
+            <translation>水深平均流速</translation>
+        </message>
+        <message>
+            <source>(1) surface velocity</source>
+            <translation>表面流速</translation>
+        </message>
+        <message>
+            <source>(2) bottom velocity</source>
+            <translation>底面流速</translation>
+        </message>
+        <message>
+            <source>Periodic boundary condition</source>
+            <translation>周期境界条件</translation>
+        </message>
+        <message>
+            <source>Minimum depth of tracer stopping(m)</source>
+            <translation>トレーサーの移動が停止する最小(限界)水深(m)</translation>
+        </message>
+        <message>
+            <source>Delta T for Particle Tracking</source>
+            <translation>トレーサー追跡の時間刻み(秒)</translation>
+        </message>
+        <message>
+            <source>Computation Finishing Time(sec)</source>
+            <translation>計算終了時間(秒)</translation>
+        </message>
+        <message>
+            <source>Output frequency increase factor</source>
+            <translation>読み込んだ流れ場情報の時間刻みよりさらに細かいトレーサー情報を出力したい場合の倍率係数</translation>
+        </message>
+        <message>
+            <source>Normal Tracers Supplying Condition</source>
+            <translation>通常トレーサーの供給条件</translation>
+        </message>
+        <message>
+            <source>Parameters for Normal Tracer Injection</source>
+            <translation>投入位置情報</translation>
+        </message>
+        <message>
+            <source>Xi(Streamwise) Direction</source>
+            <translation>流下方向(ξ=0～1)</translation>
+        </message>
+        <message>
+            <source>xi_1</source>
+            <translation>開始相対位置(0～1)</translation>
+        </message>
+        <message>
+            <source>xi_2</source>
+            <translation>終了相対位置(0～1)</translation>
+        </message>
+        <message>
+            <source>d_trace_xi</source>
+            <translation>相対間隔刻み</translation>
+        </message>
+        <message>
+            <source>Eta(Transverse) Direction</source>
+            <translation>横断方向(η=0～1)</translation>
+        </message>
+        <message>
+            <source>eta_1</source>
+            <translation>開始相対位置(0～1)</translation>
+        </message>
+        <message>
+            <source>eta_2</source>
+            <translation>終了相対位置(0～1)</translation>
+        </message>
+        <message>
+            <source>d_trace_eta</source>
+            <translation>相対間隔刻み</translation>
+        </message>
+        <message>
+            <source>Time Parameters(Seconds)</source>
+            <translation>投入時間情報(秒)</translation>
+        </message>
+        <message>
+            <source>spread_start</source>
+            <translation>開始時間</translation>
+        </message>
+        <message>
+            <source>spread_end</source>
+            <translation>終了時間</translation>
+        </message>
+        <message>
+            <source>spred_interval</source>
+            <translation>時間間隔</translation>
+        </message>
+        <message>
+            <source>Special Tracers for Trajectory Supping Condition</source>
+            <translation>軌跡表示を行う特別トレーサー投入条件</translation>
+        </message>
+        <message>
+            <source>Parameters for Special Tracers for Trajectory Injection</source>
+            <translation>特別トレーサー</translation>
+        </message>
+        <message>
+            <source>xi_1_tj</source>
+            <translation>ξ方向の投入開始相対位置(0～1)</translation>
+        </message>
+        <message>
+            <source>xi_2_tj</source>
+            <translation>ξ方向の投入終了相対位置(0～1)</translation>
+        </message>
+        <message>
+            <source>d_trace_xi_tj</source>
+            <translation>投入間隔</translation>
+        </message>
+        <message>
+            <source>eta_1_tj</source>
+            <translation>η方向の投入開始相対位置(0～1)</translation>
+        </message>
+        <message>
+            <source>eta_2_tj</source>
+            <translation>η方向の投入終了相対位置(0～1)</translation>
+        </message>
+        <message>
+            <source>d_trace_eta_tj</source>
+            <translation>投入間隔</translation>
+        </message>
+        <message>
+            <source>Time of Special Tracers Injection(Seconds)</source>
+            <translation>特別トレーサーの投入時間(注)一発のみ</translation>
+        </message>
+        <message>
+            <source>spread__tj</source>
+            <translation>投入時間(秒)</translation>
+        </message>
+        <message>
+            <source>Background Flow Condition</source>
+            <translation>トレーサー追跡に使用する水理量</translation>
+        </message>
+        <message>
+            <source>Back Ground Flow Velocity Condition</source>
+            <translation>使用する流速</translation>
+        </message>
+        <message>
+            <source>(0) Use Simulated Value</source>
+            <translation>他のソルバによって計算されたCGNSファイルから読む</translation>
+        </message>
+        <message>
+            <source>(1) Give Constant Value</source>
+            <translation>一定の流速を使用する</translation>
+        </message>
+        <message>
+            <source>Background Flow Velocity(m/s)</source>
+            <translation>背景の一定流速値</translation>
+        </message>
+        <message>
+            <source>Background Depth(m)</source>
+            <translation>背景の一定水深</translation>
+        </message>
+        <message>
+            <source>Tracer Cloning and Amalgamation</source>
+            <translation>トレーサーのクローニング(分割)と再結合</translation>
+        </message>
+        <message>
+            <source>Tracer Cloning (Only for Normal Tracers)</source>
+            <translation>クローニング(通常トレーサーのみ）</translation>
+        </message>
+        <message>
+            <source>(0)No</source>
+            <translation>しない</translation>
+        </message>
+        <message>
+            <source>(1)Yes</source>
+            <translation>する</translation>
+        </message>
+        <message>
+            <source>Cloning Method</source>
+            <translation>クローニングの方法</translation>
+        </message>
+        <message>
+            <source>Cloning condition or method</source>
+            <translation>方法の選択</translation>
+        </message>
+        <message>
+            <source>All empty cells</source>
+            <translation>トレーサーの無いセルはすべて発生させる</translation>
+        </message>
+        <message>
+            <source>Cells where only one particle</source>
+            <translation>トレーサーが１個だけのセル</translation>
+        </message>
+        <message>
+            <source>Specified cells with one particle</source>
+            <translation>あらかじめ指定した領域</translation>
+        </message>
+        <message>
+            <source>Maximum Cloning Generations</source>
+            <translation>最大のクローニング世代数</translation>
+        </message>
+        <message>
+            <source>Amalgamation</source>
+            <translation>再結合</translation>
+        </message>
+        <message>
+            <source>Amalgamation (Only for Normal Tracers)</source>
+            <translation>通常トレーサーのみ</translation>
+        </message>
+        <message>
+            <source>No</source>
+            <translation>しない</translation>
+        </message>
+        <message>
+            <source>Yes</source>
+            <translation>する</translation>
+        </message>
+        <message>
+            <source>Number of particle at which colligation is executed</source>
+            <translation>再結合開始限界セル内トレーサー数(この数に達したら再結合を開始する)</translation>
+        </message>
+        <message>
+            <source>Computation of Fish Motion</source>
+            <translation>魚の移動追跡</translation>
+        </message>
+        <message>
+            <source>Fish_Motion</source>
+            <translation>条件</translation>
+        </message>
+        <message>
+            <source>Computation of Fish</source>
+            <translation>計算</translation>
+        </message>
+        <message>
+            <source>Fish Cruising Speed(m/s)</source>
+            <translation>巡行速度(m/s)</translation>
+        </message>
+        <message>
+            <source>Cruise time VS fish length ration(s/m)</source>
+            <translation>巡行可能時間が体長に比例するとした場合の比例定数(s/m)</translation>
+        </message>
+        <message>
+            <source>Rush time ratio to cruising cycle time</source>
+            <translation>突進時間の巡行時間に対する割合</translation>
+        </message>
+        <message>
+            <source>Rush speed ratio to cruising speed</source>
+            <translation>突進速度の巡行速度に対する割合</translation>
+        </message>
+        <message>
+            <source>Fish velocity variation</source>
+            <translation>魚の速度のばらつき</translation>
+        </message>
+        <message>
+            <source>(0) Constant velocity</source>
+            <translation>一定とする</translation>
+        </message>
+        <message>
+            <source>(1) Variation velocity</source>
+            <translation>ばらつきを持たせる</translation>
+        </message>
+        <message>
+            <source>Averaged Fish Body Length(m)</source>
+            <translation>平均体長(m)</translation>
+        </message>
+        <message>
+            <source>Fish length variation</source>
+            <translation>体長のバラつき</translation>
+        </message>
+        <message>
+            <source>(0) Constant length</source>
+            <translation>一定</translation>
+        </message>
+        <message>
+            <source>(1) Variation length</source>
+            <translation>ばらつきを持たせる</translation>
+        </message>
+        <message>
+            <source>Numbers of Fish Size(Color)</source>
+            <translation>体長の種類数(色別)</translation>
+        </message>
+        <message>
+            <source>xi_1_fish</source>
+            <translation>放流開始流下方向相対位置(ξ1)</translation>
+        </message>
+        <message>
+            <source>xi_2_fish</source>
+            <translation>放流終了流下方向相対位置(ξ2)</translation>
+        </message>
+        <message>
+            <source>d_xi_fish</source>
+            <translation>上記相対間隔</translation>
+        </message>
+        <message>
+            <source>eta_1_fish</source>
+            <translation>放流開始横断方向相対位置(η1)</translation>
+        </message>
+        <message>
+            <source>eta_2_fish</source>
+            <translation>放流終了横断方向相対位置(η2)</translation>
+        </message>
+        <message>
+            <source>d_eta_fish</source>
+            <translation>上記相対間隔</translation>
+        </message>
+        <message>
+            <source>Diffusion Condition</source>
+            <translation>乱れの影響</translation>
+        </message>
+        <message>
+            <source>Diffusivity Correction</source>
+            <translation>ランダムウォークによるセル以下スケールの乱れ考慮</translation>
+        </message>
+        <message>
+            <source>Diffusivity Parameters</source>
+            <translation>パラメーター</translation>
+        </message>
+        <message>
+            <source>A value in D=A Nu + B</source>
+            <translation>D=Aμ+BにおけるAの値</translation>
+        </message>
+        <message>
+            <source>B value (m2 s-1)</source>
+            <translation>Bの値</translation>
+        </message>
+        <message>
+            <source>Elevation(m)</source>
+            <translation>河床高(m)</translation>
+        </message>
+        <message>
+            <source>Obstacle</source>
+            <translation>障害物</translation>
+        </message>
+        <message>
+            <source>Normal cell</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>Tracer Cloning Cell</source>
+            <translation>トレーサーのクローニングを行うセル</translation>
+        </message>
+        <message>
+            <source>Normal Cell</source>
+            <translation>通常セル</translation>
+        </message>
+        <message>
+            <source>Tracer Amalgamation Cell</source>
+            <translation>トレーサーの再結合を行うセル</translation>
+        </message>
+        <message>
+            <source>Manning's roughness coefficient</source>
+            <translation>マニングの粗度係数</translation>
+        </message>
+    </context>
+</TS>
---
+++ prod_src/packages/solver.nays2dv/data/translation_ja_JP.ts
@@ -0,0 +1,352 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TS version="2.0" language="ja_JP">
+    <!--これは iRIC で使用する辞書ファイルです。
+<translation> と </translation> の間に、翻訳後の文字列を追記してください。
+-->
+    <context>
+        <message>
+            <source>Nasy2dv vertical 2D model </source>
+            <translation>Nays2dv 簡単鉛直2次密度流モデル(密度流でなくても計算できます)</translation>
+        </message>
+        <message>
+            <source>Computational Parameters</source>
+            <translation>計算パラメーター</translation>
+        </message>
+        <message>
+            <source>Finite Differential Method</source>
+            <translation>計算方式</translation>
+        </message>
+        <message>
+            <source>Velocity Advection Term</source>
+            <translation>流速の移流項</translation>
+        </message>
+        <message>
+            <source>CIP Method</source>
+            <translation>CIP法</translation>
+        </message>
+        <message>
+            <source>Upwind Method</source>
+            <translation>風上差分</translation>
+        </message>
+        <message>
+            <source>Boundary Condition</source>
+            <translation>境界条件</translation>
+        </message>
+        <message>
+            <source>Upstream and Down Stream Boundary</source>
+            <translation>上下流の境界条件</translation>
+        </message>
+        <message>
+            <source>Closed channel</source>
+            <translation>上下流閉鎖境界</translation>
+        </message>
+        <message>
+            <source>Open Channel</source>
+            <translation>上下流自由境界</translation>
+        </message>
+        <message>
+            <source>Upstream open and downstream closed</source>
+            <translation>上流自由境界・下流閉鎖境界</translation>
+        </message>
+        <message>
+            <source>Upstream closed and downstream open</source>
+            <translation>上流閉鎖境界・下流自由境界</translation>
+        </message>
+        <message>
+            <source>Periodic boundary condition</source>
+            <translation>周期境界条件</translation>
+        </message>
+        <message>
+            <source>Non-periodic(Free) boundary</source>
+            <translation>非周期境界</translation>
+        </message>
+        <message>
+            <source>Periodic boundary</source>
+            <translation>周期境界</translation>
+        </message>
+        <message>
+            <source>Initial Discharge per Unit Width</source>
+            <translation>初期単位幅流量</translation>
+        </message>
+        <message>
+            <source>Downstream Water Surface</source>
+            <translation>下流端水位</translation>
+        </message>
+        <message>
+            <source>Downstream Water Surface Condition</source>
+            <translation>下流端水位条件</translation>
+        </message>
+        <message>
+            <source>Constant</source>
+            <translation>一定</translation>
+        </message>
+        <message>
+            <source>Sine Wave Oscillation</source>
+            <translation>サインカーブ振動</translation>
+        </message>
+        <message>
+            <source>Time Series Data Input</source>
+            <translation>時系列入力</translation>
+        </message>
+        <message>
+            <source>Horizontal(dH/dx=0)</source>
+            <translation>水平(dH/dt=0)</translation>
+        </message>
+        <message>
+            <source>Uniform Slope (d2H/dx2=0)</source>
+            <translation>2次微分ゼロ(d2H/dx2=0)</translation>
+        </message>
+        <message>
+            <source>Setting of DWSE</source>
+            <translation>下流端水位の設定</translation>
+        </message>
+        <message>
+            <source>Identical to Initial Value</source>
+            <translation>初期値と同じ</translation>
+        </message>
+        <message>
+            <source>Value Input</source>
+            <translation>値を入力</translation>
+        </message>
+        <message>
+            <source>Downstream Water Surface Elevation(m)</source>
+            <translation>下流端水位の値(m)</translation>
+        </message>
+        <message>
+            <source>Downstream Oscillation Values</source>
+            <translation>下流端水位振動パラメーター</translation>
+        </message>
+        <message>
+            <source>Amplitude(m)</source>
+            <translation>波高(m)</translation>
+        </message>
+        <message>
+            <source>Wavelength(sec)</source>
+            <translation>波長(秒)</translation>
+        </message>
+        <message>
+            <source>Start Time of Oscillation(sec)</source>
+            <translation>振動開始時間(秒)</translation>
+        </message>
+        <message>
+            <source>Time to become full oscillation(sec)</source>
+            <translation>フル振動に至るまでの時間(sec)</translation>
+        </message>
+        <message>
+            <source>Upstream Water Surface</source>
+            <translation>上流端の水位</translation>
+        </message>
+        <message>
+            <source>Upstream Water Surface Condition</source>
+            <translation>上流端水位条件</translation>
+        </message>
+        <message>
+            <source>Horizontal (dH/dx=0)</source>
+            <translation>水平</translation>
+        </message>
+        <message>
+            <source>Setting of UPSE</source>
+            <translation>水位を設定</translation>
+        </message>
+        <message>
+            <source>Upstream Water Surface Elevation(m)</source>
+            <translation>上流単水位の値(m)</translation>
+        </message>
+        <message>
+            <source>Grain Size of Bed Material(m)</source>
+            <translation>粗度としての河床材料粒径(m)</translation>
+        </message>
+        <message>
+            <source>Relative Length to the Depth of the First Grid From Bottom </source>
+            <translation>最下端の格子サイズの水深に対する割合</translation>
+        </message>
+        <message>
+            <source>Eddy Viscosity</source>
+            <translation>渦動粘性係数</translation>
+        </message>
+        <message>
+            <source>Parabolic</source>
+            <translation>放物線分布</translation>
+        </message>
+        <message>
+            <source>Coefficient for Eddy Viscosity</source>
+            <translation>渦動粘性係数の倍率</translation>
+        </message>
+        <message>
+            <source>Time and Iteration Parameters</source>
+            <translation>時間に関するパラメーター</translation>
+        </message>
+        <message>
+            <source>Time Parameters</source>
+            <translation>時間に関するパラメータ</translation>
+        </message>
+        <message>
+            <source>Output Interval(sec)</source>
+            <translation>出力時間間隔(秒)</translation>
+        </message>
+        <message>
+            <source>Computation Finishing Time(sec)</source>
+            <translation>計算終了時間(秒)</translation>
+        </message>
+        <message>
+            <source>Time Step of Computation(sec)</source>
+            <translation>計算時間刻み(秒)</translation>
+        </message>
+        <message>
+            <source>Iteration Parameters</source>
+            <translation>繰り返し計算に関するパラメーター</translation>
+        </message>
+        <message>
+            <source>Error Limitation in SOR Computation</source>
+            <translation>SOR法の打ち切り誤差</translation>
+        </message>
+        <message>
+            <source>Max. Times of Iteration</source>
+            <translation>圧力計算での最大繰り返し計算回数</translation>
+        </message>
+        <message>
+            <source>Relaxation Coefficient</source>
+            <translation>緩和係数</translation>
+        </message>
+        <message>
+            <source>Free Surface Calculation</source>
+            <translation>自由水面の計算</translation>
+        </message>
+        <message>
+            <source>No</source>
+            <translation>計算しない</translation>
+        </message>
+        <message>
+            <source>Yes</source>
+            <translation>計算する</translation>
+        </message>
+        <message>
+            <source>Relaxation Coefficient for Free Surface Computation</source>
+            <translation>自由水面計算の緩和係数</translation>
+        </message>
+        <message>
+            <source>Starting Time of Free Surface Computation</source>
+            <translation>自由水面計算の開始時間(秒)</translation>
+        </message>
+        <message>
+            <source>Iteration time for water surface</source>
+            <translation>自由水面の繰り返し計算数</translation>
+        </message>
+        <message>
+            <source>Truncation error</source>
+            <translation>打ち切り誤差</translation>
+        </message>
+        <message>
+            <source>Discharge Adjustment</source>
+            <translation>流量補正</translation>
+        </message>
+        <message>
+            <source>Discharge Adjustment Coefficient</source>
+            <translation>流量補正係数</translation>
+        </message>
+        <message>
+            <source>Discharge Adjustment Starting Time</source>
+            <translation>流量補正の開始時間(秒)</translation>
+        </message>
+        <message>
+            <source>Physical Parameters</source>
+            <translation>物理定数</translation>
+        </message>
+        <message>
+            <source>Kinematic Viscosity Coefficient</source>
+            <translation>動粘性係数</translation>
+        </message>
+        <message>
+            <source>Diffusion Coefficient for Heat</source>
+            <translation>熱による温度拡散係数</translation>
+        </message>
+        <message>
+            <source>Diffusion Coefficient for Concentration</source>
+            <translation>濃度拡散係数</translation>
+        </message>
+        <message>
+            <source>Volume expansion coefficient</source>
+            <translation>体積膨張係数</translation>
+        </message>
+        <message>
+            <source>Density(kg/m**3)</source>
+            <translation>環境(背景)流体の密度</translation>
+        </message>
+        <message>
+            <source>Surface Tension Coefficient</source>
+            <translation>表面張力係数</translation>
+        </message>
+        <message>
+            <source>Background Values</source>
+            <translation>背景(環境)条件</translation>
+        </message>
+        <message>
+            <source>Background Temperature and Concentration</source>
+            <translation>背景温度</translation>
+        </message>
+        <message>
+            <source>Use Minimum Value</source>
+            <translation>最小値を使う</translation>
+        </message>
+        <message>
+            <source>Use Maximum Value</source>
+            <translation>最大値を使う</translation>
+        </message>
+        <message>
+            <source>Use Averaged Value</source>
+            <translation>平均値を使う</translation>
+        </message>
+        <message>
+            <source>Give Specific Value</source>
+            <translation>入力する</translation>
+        </message>
+        <message>
+            <source>Back Ground Temperature</source>
+            <translation>背景温度</translation>
+        </message>
+        <message>
+            <source>Back Ground Concentration</source>
+            <translation>背景濃度</translation>
+        </message>
+        <message>
+            <source>Elevation</source>
+            <translation>河床高</translation>
+        </message>
+        <message>
+            <source>Initial Temperature (degree)</source>
+            <translation>初期温度</translation>
+        </message>
+        <message>
+            <source>Initial Concentration</source>
+            <translation>初期濃度</translation>
+        </message>
+        <message>
+            <source>Obstacle Cell</source>
+            <translation>障害物セル</translation>
+        </message>
+        <message>
+            <source>Normal cell</source>
+            <translation>通常セル</translation>
+        </message>
+        <message>
+            <source>Obstacle</source>
+            <translation>障害物</translation>
+        </message>
+        <message>
+            <source>T_Bound</source>
+            <translation>温度境界</translation>
+        </message>
+        <message>
+            <source>Temperature at Boundary</source>
+            <translation>境界温度</translation>
+        </message>
+        <message>
+            <source>C_Bound</source>
+            <translation>濃度境界</translation>
+        </message>
+        <message>
+            <source>Concentration at Boundary</source>
+            <translation>境界条件濃度</translation>
+        </message>
+    </context>
+</TS>
